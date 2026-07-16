e2a7d1caddc548f1501a1759841162fab571be59
Raise <huxyc@qq.com>
2026-07-16 16:47:09 +0800

    update

--- MergeDocxFiles/Form1.Designer.cs
+++ MergeDocxFiles/Form1.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.button4 = new System.Windows.Forms.Button();
             this.label3 = new System.Windows.Forms.Label();
             this.textBox_inCase = new System.Windows.Forms.TextBox();
@@ -113,6 +114,7 @@
             this.Controls.Add(this.button4);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.textBox_inCase);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Form1";
             this.Text = "MeigeDocxFiles for SAR Plots";
             this.groupBox1.ResumeLayout(false);
